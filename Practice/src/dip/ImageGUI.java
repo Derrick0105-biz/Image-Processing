2725a916ab7a3fef10ad5cb7f79071a445814e71
Derrick <derrick0105.cg@gmail.com>
2020-04-20 10:53:06 +0800

    test commit

--- Practice/src/dip/ImageGUI.java
+++ Practice/src/dip/ImageGUI.java
@@ -1,4 +1,14 @@
 package dip;
 
+import javax.swing.JFrame;
+
 public class ImageGUI {
+
+    public static void main(String[] args) {
+        //BasicFrame frame = new BasicFrame();
+        AffineFrame frame = new AffineFrame();
+        frame.setVisible(true);
+        frame.setSize(1500, 1500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
--- Practice/src/dip/ImagePanel.java
+++ Practice/src/dip/ImagePanel.java
@@ -1,4 +1,46 @@
 package dip;
 
-public class ImagePanel {
+//import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+import dip.Util;
+
+
+public class ImagePanel extends JPanel {
+
+
+
+
+    BufferedImage image;
+
+    public void showImage(int width, int height, int [][][] data) {
+        System.out.print("有被呼到");
+
+
+        if (width == 0 || height == 0) {
+            image = null;
+            this.repaint();
+            return;
+        }
+
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int [] rgb = data[j][i];
+                image.setRGB(i, j, Util.getRGB(rgb[0], rgb[1], rgb[2]));
+            }
+        }
+        this.repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (image != null)
+            g.drawImage(image, 0, 0, this);
+    }
 }
+
